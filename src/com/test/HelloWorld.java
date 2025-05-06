9f492b744501777c354806ce81cf126257d9b8b3
manvitha-k-v <manvithakv.93@gmail.com>
2025-05-06 09:31:54 +0530

    Update HelloWorld.java

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number);
+int n=sc.nextInt();
+int res=n*n;
+System.out.println("the square of the number is "+res);
 	}
 
 }
